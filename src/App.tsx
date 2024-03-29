f220e50d7f090480d06a2bf672c59f9bf9b8e1bb
johnpham <pltnhan311@gmail.com>
2024-03-29 18:26:45 +0700

    update full code in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,89 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import { Count } from "./Count";
 
+// type People = {
+//   name: string;
+//   age: number;
+//   location?: string;
+// };
+
+// type Employee = People & {
+//   jobTitle: string;
+//   degree: string;
+// };
+
+// type ApiResponse = {
+//   data: { id: string; name: string };
+//   status: "success" | "error";
+// };
+
+// type BookResponse = {
+//   data: { id: string; bookName: string };
+//   status: "success" | "error";
+// };
+
+type ApiResponse<DataType> = {
+  data: DataType;
+  status: "success" | "error";
+};
+interface IPeople {
+  name: string;
+  age: number;
+  location?: string;
+}
+
+enum Degree {
+  ASSOCIATES = "ASSOCIATES",
+  BACHELORS = "BACHELORS",
+  MASTERS = "MASTERS",
+  DOCTORATE = "DOCTORATE",
+  PROFESSIONAL = "PROFESSIONAL",
+}
+
+interface IEmployee extends IPeople {
+  jobTitle: string;
+  degree: Degree;
+  level?: string | number | Degree;
+}
+
 function App() {
   const [count, setCount] = useState(0);
+  const [people, setPeople] = useState<IPeople>();
 
-  function total(num1: number, num2:number): number {
+  const employee: IEmployee = {
+    name: "Alice",
+    age: 31,
+    jobTitle: "Accountant",
+    degree: Degree.DOCTORATE,
+    level: "3",
+  };
+
+  const userResponse: ApiResponse<{ id: string, name: string }> = {
+    data: { id: "1", name: "Bob" },
+    status: "success",
+  };
+
+  const bookResponse: ApiResponse<{ id: string, bookName: string }> = {
+    data: { id: "1", bookName: "The Great Gatsby" },
+    status: "error",
+  };
+
+  function total(num1: number, num2: number): number {
     return num1 + num2;
   }
+
+  useEffect(() => {
+    setPeople({
+      name: "John",
+      age: 22,
+      location: "HCM",
+    });
+  }, []);
+
+  console.log(people);
 
   return (
     <>
@@ -26,6 +100,11 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <h2>{people?.name}</h2>
+      <h2>{employee?.degree}</h2>
+      <h2>{employee?.level}</h2>
+      <h2>{userResponse.data.name}</h2>
+      <h2>{bookResponse.data.bookName}</h2>
     </>
   );
 }
